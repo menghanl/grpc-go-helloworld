0dbf7554cc0be0e355fd16bfd86d1f5445abdc23
Menghan Li <menghanl@google.com>
2020-07-07 12:04:12 -0700

    naive c/s

--- wallet/wallet_client/main.go
+++ wallet/wallet_client/main.go
@@ -1,11 +1,66 @@
+// Package main implements a client for Greeter service.
 package main
 
 import (
+	"context"
+	"flag"
 	"fmt"
+	"log"
+	"time"
 
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 	walletpb "google.golang.org/grpc/grpc-wallet/grpc/examples/wallet"
+
+	_ "google.golang.org/grpc/xds/experimental"
+)
+
+var (
+	address = flag.String("address", "localhost:50051", "server address, with port")
+	name    = flag.String("name", "world", "name to greet")
+
+	dialTimeout = flag.Duration("dialTimeout", 10*time.Second, "timeout for creating grpc.ClientConn, format: 100ms, or 10s, or 2h")
+	rpcTimeout  = flag.Duration("rpcTimeout", time.Second, "timeout for each RPC, format: 100ms, or 10s, or 2h")
+
+	watch = flag.Bool("watch", false, "streaming watch or not")
 )
 
 func main() {
-	fmt.Printf("hello wallet, %+v\n", walletpb.BalancePerAddress{Address: "abc", Balance: 123})
+	flag.Parse()
+	// Set up a connection to the server.
+	ctx, cancel := context.WithTimeout(context.Background(), *dialTimeout)
+	defer cancel()
+
+	addr := *address
+
+	conn, err := grpc.DialContext(ctx, addr, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := walletpb.NewWalletClient(conn)
+
+	if !*watch {
+		var header metadata.MD
+		ctx, cancel := context.WithTimeout(context.Background(), *rpcTimeout)
+		defer cancel()
+		r, err := c.FetchBalance(ctx, &walletpb.BalanceRequest{}, grpc.Header(&header))
+		if err != nil {
+			log.Fatalf("could not fetch: %v", err)
+		}
+		fmt.Printf("Fetch: %v, from %v\n", r, header["hostname"])
+		return
+	}
+
+	{
+		ctx, cancel := context.WithTimeout(context.Background(), *rpcTimeout)
+		defer cancel()
+		s, err := c.WatchBalance(ctx, &walletpb.BalanceRequest{})
+		if err != nil {
+			log.Fatalf("could not fetch: %v", err)
+		}
+		header, _ := s.Header()
+		r, _ := s.Recv()
+		fmt.Printf("Watch: %v, from %v\n", r, header["hostname"])
+	}
 }
--- wallet/wallet_server/main.go
+++ wallet/wallet_server/main.go
@@ -1,11 +1,47 @@
+// server ...
 package main
 
 import (
+	"context"
+	"flag"
 	"fmt"
+	"log"
+	"net"
+	"os"
 
+	"google.golang.org/grpc"
 	walletpb "google.golang.org/grpc/grpc-wallet/grpc/examples/wallet"
+	"google.golang.org/grpc/metadata"
 )
 
+var (
+	port        = flag.Int("port", 50051, "The server port")
+	serverID    = flag.String("id", "not-set", "")
+	hostname, _ = os.Hostname()
+)
+
+type server struct{}
+
+func (s *server) FetchBalance(ctx context.Context, _ *walletpb.BalanceRequest) (*walletpb.BalanceResponse, error) {
+	grpc.SetHeader(ctx, metadata.Pairs("hostname", hostname, "hostname", *serverID))
+	return &walletpb.BalanceResponse{Balance: 123}, nil
+}
+
+func (s *server) WatchBalance(_ *walletpb.BalanceRequest, stream walletpb.Wallet_WatchBalanceServer) error {
+	stream.SetHeader(metadata.Pairs("hostname", hostname, "hostname", *serverID))
+	stream.Send(&walletpb.BalanceResponse{Balance: 456})
+	return nil
+}
+
 func main() {
-	fmt.Printf("hello wallet, %+v\n", walletpb.BalancePerAddress{Address: "abc", Balance: 123})
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	walletpb.RegisterWalletServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
